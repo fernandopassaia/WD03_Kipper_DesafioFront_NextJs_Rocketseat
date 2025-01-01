9c4c6193b7d8bf0c5209e4fdc1c3ec829c0af7b4
Fernando Passaia <fernandopassaia@futuradata.com.br>
2025-01-01 20:16:47 +0000

    Swapping environment

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -8,10 +8,12 @@ import { useDeferredValue } from "react";
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
+  console.log('API_URL Fetcher', API_URL);
   return axios.post(API_URL,{ query })
 }
 
 export function useProducts(){
+  console.log('API_URL Products', API_URL);
     const { type, priority, search } = useFilter()
     const searchDeferred = useDeferredValue(search)
     const query = mountQuery(type, priority)
@@ -20,9 +22,10 @@ export function useProducts(){
       queryKey: ['products', type, priority]
     })
 
+    console.log('Data', data);
     const products =  data?.data?.data?.allProducts
     const filteredProducts = products?.filter(product => product.name.toLowerCase().includes(searchDeferred.toLowerCase()))
-
+    console.log('filteredProducts', filteredProducts);
     return {
       data: filteredProducts
     }
